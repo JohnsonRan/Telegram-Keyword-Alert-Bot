bd7cba1bae76175966311b135b9b9fe447d1711b
JohnsonRan <Johnson_Ran@hotmail.com>
2023-11-12 04:31:00 +0800

    add /check command

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import asyncio
 from telethon import TelegramClient, events
 import telebot
 import logging
+import os
+import psutil
+from datetime import timedelta
+import datetime
+import subprocess
+import yaml
+import threading
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
-# Configuration
-config = {
-    'api_id': 'YOUR_API_ID',  # Replace with your API ID
-    'api_hash': 'YOUR_API_HASH',  # Replace with your API hash
-    'user_id': YOUR_USER_ID,  # Replace with your user ID
-    'keywords': ['test', 'test1'],  # Replace with your keywords
-    'channel_link': YOUR_CHANNEL_ID,  # Replace with your channel ID
-    'bot_token': 'YOUR_BOT_TOKEN'  # Replace with your bot token
-}
+# Load the configuration from the YAML file
+with open('config.yml', 'r') as f:
+    config = yaml.safe_load(f)
 
 client = TelegramClient('anon', config['api_id'], config['api_hash'])
 bot = telebot.TeleBot(config['bot_token'])
@@ -27,15 +28,44 @@ async def my_event_handler(event):
             message_link = f"https://t.me/c/{config['channel_link']}/{event.id}"
             bot.send_message(config['user_id'], f"关键词: {keyword}\n链接: {message_link}")
 
+@bot.message_handler(commands=['check'])
+def send_system_info(message):
+    logging.info("Received /check command")  # Log a message when the /check command is received
+
+    # 获取系统运行时间
+    uptime = subprocess.check_output(['uptime', '-p']).decode('utf-8').strip()
+
+    # 获取CPU使用情况
+    cpu_usage = psutil.cpu_percent(interval=1)
+
+    # 获取内存使用情况
+    memory_info = psutil.virtual_memory()
+    memory_usage = memory_info.percent
+
+    # 获取服务器时间
+    server_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # 构建消息
+    msg = f"服务器运行时间: {uptime}\nCPU使用率: {cpu_usage}%\n内存使用率: {memory_usage}%\n服务器时间: {server_time}"
+
+    # 发送消息
+    bot.reply_to(message, msg)
+
+def start_polling():
+    bot.polling()
+
 async def main():
     try:
         logging.info("Starting client...")
         await client.start()
         logging.info("Client started. Running until disconnected...")
+        threading.Thread(target=start_polling).start()  # Start polling in a new thread
         await client.run_until_disconnected()
     except Exception as e:
         logging.error(f"An error occurred: {e}")
     finally:
+        logging.info("Stopping bot polling...")
+        bot.stop_polling()
         logging.info("Logging out...")
         await client.log_out()
         logging.info("Logged out.")
