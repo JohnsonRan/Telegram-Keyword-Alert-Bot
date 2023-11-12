4d7b8123026d32c781ae400b6c15310ca03b06c8
JohnsonRan <Johnson_Ran@hotmail.com>
2023-11-12 11:51:53 +0800

    add /check command
    support check multiple channels

--- main.py
+++ main.py
@@ -20,22 +20,38 @@ with open('config.yml', 'r') as f:
 client = TelegramClient('anon', config['api_id'], config['api_hash'])
 bot = telebot.TeleBot(config['bot_token'])
 
-# Initialize the counter
-message_counter = 0
+# Constants
+TELEGRAM_LINK_PREFIX = "https://t.me/c/"
 
-@client.on(events.NewMessage(chats=config['channel_link']))
-async def my_event_handler(event):
-    global message_counter
-    message_counter += 1  # Increment the counter for each new message
-    for keyword in config['keywords']:
-        if keyword in event.raw_text:
-            logging.info("Keyword found in message. Sending message...")
-            message_link = f"https://t.me/c/{config['channel_link']}/{event.id}"
-            bot.send_message(config['user_id'], f"关键词: {keyword}\n链接: {message_link}")
+class MessageCounter:
+    def __init__(self):
+        self.counter = {}
+
+    def increment(self, channel_title, channel_link):
+        if channel_title not in self.counter:
+            self.counter[channel_title] = {'count': 0, 'link': channel_link}
+        self.counter[channel_title]['count'] += 1
+
+    def get_counts(self):
+        return self.counter
+
+message_counter = MessageCounter()
+
+# Register an event handler for each channel
+for channel_link in config['channel_links']:
+    @client.on(events.NewMessage(chats=channel_link))
+    async def handle_new_message(event):
+        # Get the channel's information
+        channel = await event.get_chat()
+        message_counter.increment(channel.title, channel_link)
+        for keyword in config['keywords']:
+            if keyword in event.raw_text:
+                logging.info("Keyword found in message. Sending message...")
+                message_link = f"{TELEGRAM_LINK_PREFIX}{channel_link}/{event.id}"
+                bot.send_message(config['user_id'], f"关键词: {keyword}\n链接: {message_link}")
 
 @bot.message_handler(commands=['check'])
 def send_system_info(message):
-    global message_counter
     logging.info("Received /check command")  # Log a message when the /check command is received
 
     # 获取系统运行时间
@@ -52,10 +68,21 @@ def send_system_info(message):
     server_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     # 构建消息
-    msg = f"服务器运行时间: {uptime}\nCPU使用率: {cpu_usage}%\n内存使用率: {memory_usage}%\n服务器时间: {server_time}\n已检查消息数: {message_counter}"
+    msg = f"服务器运行时间: {uptime}\nCPU使用率: {cpu_usage}%\n内存使用率: {memory_usage}%\n服务器时间: {server_time}"
 
     # 发送消息
     bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['count'])
+def send_message_counts(message):
+    logging.info("Received /count command")  # Log a message when the /count command is received
+
+    # Build the message
+    counts = message_counter.get_counts()
+    msg = "已检查消息数：\n" + "\n".join(f"[{channel_name}]({TELEGRAM_LINK_PREFIX}{info['link']}): {info['count']}" for channel_name, info in counts.items())
+
+    # Send the message
+    bot.reply_to(message, msg, parse_mode='Markdown')
 
 def start_polling():
     bot.polling()
